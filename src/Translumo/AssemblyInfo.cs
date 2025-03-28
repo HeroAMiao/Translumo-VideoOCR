570301c1500460c1c8bcd2187d36360bd8b3c897
amiao <hero_amiao@protonmail.com>
2025-03-28 14:44:00 +0800

    [chore] bump assembly version to 0.11.0

--- src/Translumo/AssemblyInfo.cs
+++ src/Translumo/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Windows;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
-[assembly: AssemblyFileVersion("0.10.0")]
-[assembly: AssemblyInformationalVersion("0.10.0")]
-[assembly: AssemblyVersion("0.10.0")]
+[assembly: AssemblyFileVersion("0.11.0")]
+[assembly: AssemblyInformationalVersion("0.11.0")]
+[assembly: AssemblyVersion("0.11.0")]
 
 [assembly: AssemblyCompany("Translumo")]
 [assembly: AssemblyProduct("Translumo")]
